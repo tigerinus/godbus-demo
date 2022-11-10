81a250400b85f0eb23e2d0f33220d88b4ef55071
Tiger Wang <tigerwang@outlook.com>
2022-11-09 23:18:45 -0500

    add initial implementation (#1)

--- main.go
+++ main.go
@@ -1,30 +1,58 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"os"
+	"text/tabwriter"
+
 	"github.com/godbus/dbus/v5"
 )
 
 func main() {
+	println("godbus-demo")
+
 	conn, err := dbus.ConnectSystemBus()
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
 
-	rules := []string{
-		"member='Notify',path='/org/freedesktop/UDisks2'",
+	matchRules := []string{
+		"path_namespace='/'",
 	}
 
 	var flag uint
 
-	call := conn.BusObject().Call("org.freedesktop.Monitoring.BecomeMonitor", 0, rules, flag)
+	println("trying to become a monitor...")
+	call := conn.BusObject().Call("org.freedesktop.DBus.Monitoring.BecomeMonitor", 0, matchRules, flag)
 	if call.Err != nil {
 		panic(call.Err)
 	}
 
+	println("eavesdropping to channel...")
 	ch := make(chan *dbus.Message, 10)
 	conn.Eavesdrop(ch)
-	for v := range ch {
-		println(v)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+	for {
+		select {
+		case <-ctx.Done():
+		case msg, ok := <-ch:
+			if !ok {
+				println("channel closed")
+				return
+			}
+
+			fmt.Fprintf(w, "type:\t%v\n", msg.Type)
+			fmt.Fprintf(w, "flags:\t%v\n", msg.Flags)
+			fmt.Fprintf(w, "headers:\t%v\n", msg.Headers)
+			fmt.Fprintf(w, "body:\t%v\n", msg.Body)
+			fmt.Fprintf(w, "---\n")
+			w.Flush()
+		}
 	}
 }
